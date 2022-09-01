30a5322d471580274325e3746b0ccd87aae96c9b
unknown <traicaydacnhiem@gmail.com>
2022-09-02 00:33:33 +0700

    testing release

---
+++ README.md
@@ -0,0 +1,34 @@
+# Lanota Score Calculator
+A simple tool to calculate the score achieved in the rhythm game, Lanota.
+## How the scoring system works
+As in the case of many other rhythm games such as Cytus, Deemo, etc., Lanota also features a **combo score** system, but with some twists.
+### Basic values
+**Base note score**: 920 000 points are split equally between all notes in the song.
+A Tune note is worth only 10/23 (approx. 43.475%) of a Harmony note's base value. Combo score is not modified by this rule. 
+**Combo score rules**:
+1. Unlike Deemo or Cytus where combo score increases over time as your combo grows, the combo score actually *decreases* in this game.
+Thus, the first note hit in every song worths the most points in any chart.
+Each following notes will have its value decreased by one **combo score unit**, up to half the note count, at which point the combo score will quickly decrease to a constant value, depending on the note count:
+- If the note count is odd, it is decreased quickly over the next two notes,
+- otherwise, it is decreased quickly over the next note.
+2. If your current combo is less than or equal to your max combo achieved in a specific song, you won't get any combo score.
+E.g. You achieved a combo of 200 before Failing a note. The next note hit will not award combo score, regardless of whether it is a Harmony or Tune note.
+3. The combo score's decreasing process carries over if you break a combo, but resumed it later.
+E.g. You achieved a combo of 5 before Failing a note. As per the rule mentioned in **1.**, the last note hit in the previous combo has its combo score decreased by four **combo score units**. If you resume a combo later, as your combo counter hits 6, that very note will have its combo score decreased by five **combo score units**.
+### Detailed score formulae:
+**Base note score:** $\920000 \over (note count)$ [1]
+**Combo score unit:** $\combo\_score\_unit = \frac{160000}{(note count)^2}$ [2]
+**Difference between the maximum and minimum points awarded by a note:** $\160000 \over (note count)$ [3]
+**First note score:** Equals to the sum of [1] and [3], minus 0.5 **combo score units**.
+$$\first\_note\_score = \frac{920000}{note\_count}+\frac{160000}{note\_count}-\frac{80000}{note\_count^2}$$ [4]
+**Following note scores:** Equals to [4] minus an appropriate multiple of **combo score unit**.
+$$\note\_score_i = first\_note\_score - i \times combo\_score\_unit\\where\text{ i} \leq \lfloor note\_count \div 2\rfloor$$ [5]
+**Rapid combo score decrement:**
+If note count is odd:
+- the next note's score is decreased by $\d_1 \times combo\_score\_unit$,
+- the note following that one has its score decreased by $\d_2 \times combo\_score\_unit$;
+where
+$$\\begin{cases}d_1 + d_2 = cff\\d_1 - d_2 = 0.75\\cff = (note\_count) \div 4 + 1\end{cases}$$ [6]
+otherwise:
+- the next note's score is decreased by $\d \times combo\_score\_unit$,
+where $$\d = (note\_count) \div 4 + 0.5$$
---
+++ lanota.py
@@ -0,0 +1,71 @@
+import math
+from decimal import *
+
+
+getcontext().prec = 14
+f = open("notedata.csv", "w", encoding='utf-8')
+note = int(input())
+# init values
+base_note = Decimal(920000 / note)
+combo_diff = Decimal(160000 / note)
+# write point values
+f.write("note count,," + str(note))
+f.write("\nbase note score,," + str(base_note))
+# tune ~ 43.444444...%
+f.write("\ntune note score,," + str(base_note * Decimal(10/23)))
+f.write("\ncombo score unit,," + str(Decimal(combo_diff / note)) + "\n")
+# half the combo unit is deducted from first note
+first_note = Decimal(base_note + combo_diff - combo_diff / note / 2)
+# write that down
+f.write('"cumulative score data, might include ±1 due to rounding:"\n')
+f.write("1," + str(first_note) + "," + str(first_note) + "," + str(int(first_note)) + "\n")
+# from now to half the song size rounded down, one combo unit is deducted per note
+half_size = math.ceil(note / 2)
+if note % 2 == 0:
+    half_size += 1
+cumul = first_note
+for i in range (2, half_size):
+    first_note -= Decimal(combo_diff / note)
+    cumul += Decimal(first_note)
+    f.write(str(i) + "," + str(first_note) + "," + str(cumul) + "," + str(int(cumul)) + "\n")
+# if note count is odd,
+# the point value is quickly deducted over the next two notes
+# so that the remainder is 25% of total combo score
+# this is still affected by combo unit rules
+# i.e. the first deduction is 0.75 units larger than the second
+if note % 2 == 1:
+    # get the decrement coefficient
+    # equal to 1/4 chart size + 1
+    cff = Decimal(note / Decimal(4.0) + 1)
+    # get two deductions
+    d1 = Decimal((cff + Decimal(0.75)) / 2)
+    d2 = Decimal(cff - d1)
+    # first deduction in place
+    first_note -= Decimal(combo_diff / note * d1)
+    cumul += Decimal(first_note)
+    f.write(str(half_size) + "," + str(first_note) + "," + str(cumul) + "," + str(int(cumul)) + "\n")
+    # same for the next
+    first_note -= Decimal(combo_diff / note * d2)
+    cumul += Decimal(first_note)
+    f.write(str(half_size+1) + "," + str(first_note) + "," + str(cumul) + "," + str(int(cumul)) + "\n")
+    # from now on till score is 1000000, note score no longer changes
+    for i in range (half_size + 2, note + 1):
+        cumul += Decimal(first_note)
+        # larger chart size may cause 999999.9999999xx ~ 999999 like in dynamix
+        if i != note:
+            f.write(str(i) + "," + str(first_note) + "," + str(cumul) + "," + str(int(cumul)) + "\n")
+        else:
+            f.write(str(i) + "," + str(first_note) + "," + str(cumul) + ",1000000\n")
+# if even, the difference is inflicted wholly at the next note
+else:
+    d = Decimal(note / Decimal(4.0)) + Decimal(0.5)
+    # everything else is the same
+    first_note -= Decimal(combo_diff / note * d)
+    cumul += Decimal(first_note)
+    f.write(str(half_size) + "," + str(first_note) + "," + str(cumul) + "," + str(int(cumul)) + "\n")
+    for i in range (half_size + 1, note + 1):
+        cumul += Decimal(first_note)
+        if i != note:
+            f.write(str(i) + "," + str(first_note) + "," + str(cumul) + "," + str(int(cumul)) + "\n")
+        else:
+            f.write(str(i) + "," + str(first_note) + "," + str(cumul) + ",1000000\n")
